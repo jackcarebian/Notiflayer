94847b96cc5a111cd24721724701f010969b562b
jimmy tjahyono <jimmy.tjahyono@gmail.com>
2025-06-27 18:36:09 +0000

    Kecilkan ideal ukuran font branding halaman landing page utama.

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -5,7 +5,7 @@ export function Hero() {
   return (
     <section className="py-20 md:py-32">
       <div className="container text-center">
-        <h1 className="text-4xl font-bold font-headline tracking-tight md:text-6xl lg:text-7xl">
+        <h1 className="text-4xl font-bold font-headline tracking-tight md:text-5xl lg:text-6xl">
           Semua yang Anda butuhkan untuk menjalankan kampanye promosi yang berhasil.
         </h1>
         <p className="mt-6 max-w-2xl mx-auto text-lg text-foreground/80 md:text-xl">
